4018667f9d6bbe59f85f0e9d7466d507b1235a0d
icondenimStore <tn696199@gmail.com>
2025-09-15 00:30:33 +0700

    delete

--- EnglishApp/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.ndnt.englishapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-    opens com.ndnt.englishapp to javafx.fxml;
-    exports com.ndnt.englishapp;
-    exports com.ndnt.pojo;
-}
